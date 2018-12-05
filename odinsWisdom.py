b189b1641a290ac5fc42e5a4577b38e405f05a62
Abkara <atabk@utexas.edu>
2018-12-04 18:13:30 -0600

    added -norse flag

--- odinsWisdom.py
+++ odinsWisdom.py
@@ -4,12 +4,16 @@ import numpy as np
 import argparse
 
 parser = argparse.ArgumentParser(description='Print a random stanza of Gestathattr.')
-parser.add_argument('-norse', nargs='+')
+parser.add_argument('-norse',help="prints norse stanza instead of english",action='store_true')
+args=parser.parse_args()
 a = os.getcwd()
 pattern = re.compile('\d')
 norfile = a + '/Gestathattr.txt'
 engfile = a + '/GestathattrEnglish.txt'
-
+if args.norse:
+	file = norfile
+else:
+	file = engfile
 linenumber = []
 with open(file) as opfile:
 	for num, line in list(enumerate(opfile,1)):
